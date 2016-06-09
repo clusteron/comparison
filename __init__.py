3b26426458f2b59e0e25aad0cf0a52eace38cbd8
Ricardo Ogando <ricardogando@gmail.com>
2016-06-09 17:56:12 -0300

    More details about packages

--- __init__.py
+++ __init__.py
@@ -3,13 +3,29 @@ This is a prototype of cluster comparison code
 
 '''
 
+import sys
+
+import astropy
+
+
 # read two or more cluster catalogs
+# what are the catalogs? 
+# how to access catalogs name? config file? sys.argv? or equivalent?
+
+# use astropy Table to read catalog content
 
 
-# perform 2D RA, Dec match for a given configurable physical radius (Mpc)
 
-# assess projection effects and miscentering
+# perform 2D RA, Dec match for a given configurable physical radius 
+# (1 Mpc default)
+# turn redshift in distance using cosmology (astropy.cosmology)
+# astropy also for matches
+
+# assess projection effects and miscentering (matplotlib)
+# delta ra,dec distribution
 # plot comparing photo-zs
+
+# -------------------
 
 # perform 3D RA, Dec, z match (configurable delta z)
 

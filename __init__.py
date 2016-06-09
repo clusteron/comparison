1cfe897c06e6c4cc5685aa623e7f02aa36db2dac
Ricardo Ogando <ricardogando@gmail.com>
2016-06-09 17:40:10 -0300

    Prototype

---
+++ __init__.py
@@ -0,0 +1,17 @@
+'''
+This is a prototype of cluster comparison code
+
+'''
+
+# read two or more cluster catalogs
+
+
+# perform 2D RA, Dec match for a given configurable physical radius (Mpc)
+
+# assess projection effects and miscentering
+# plot comparing photo-zs
+
+# perform 3D RA, Dec, z match (configurable delta z)
+
+# plot comparing richness
+

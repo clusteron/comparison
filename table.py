e33fc8df302ac199525547a3dc62060cd0d6e0ed
Isabella138 <isabella14@astro.ufrj.br>
2016-06-28 15:37:52 -0300

    reformulando o codigo

--- table.py
+++ table.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy
@@ -8,65 +9,50 @@ from astropy import units as u
 import pylab
 from pylab import plot
 from astropy.cosmology import WMAP9 as cosmo
-from astropy.cosmology import FlatLambdaCDM
 
 print 'Reading files'
-w = ascii.read('wen2012.csv') # leitura do catalogo wen
-tr = fits.open('redmapper.fits') # leitura do catalogo redmapper
-r = tr[1].data
+w = ascii.read('wen.csv') 
+r = fits.open('redmapper.fits') 
+r = r[1].data
 
-#print w #imprimi o catalogo wen
-#print r # imprimi o catalogo redmapper
+#print w 
+#print r 
 
-c_w = SkyCoord(ra = w['col2']*u.degree, dec = w['col3']*u.degree) # Pega apenas os valores de ra e dec do catalogo wen
-c_r = SkyCoord(ra = r['RA']*u.degree, dec = r['DEC']*u.degree) # Pega apenas os valores de ra e dec do catalogo redmapper
-
-print 'match to catalog sky'
-idx, d2d, d3d = c_w.match_to_catalog_sky(c_r) # mostra os matches do catalogo wen em relacao ao redmapper
-#print idx, d2d, d3d #imprimi os valores
-plot(d2d,d3d) # grafico do wen em relacao ao redmapper
-pylab.xlabel('angle') #legenda pro eixo x
-pylab.ylabel('quantity') #legenda pro eixo y
-pylab.show() # mostra o grafico
-# savefig
+print 'catalog with values RA and DEC'
+cw = SkyCoord(ra = w['RAJ2000']*u.degree, dec = w['DEJ2000']*u.degree) 
+cr = SkyCoord(ra = r['RA']*u.degree, dec = r['DEC']*u.degree) 
 
 print 'match to catalog sky'
-idx, d2d, d3d = c_r.match_to_catalog_sky(c_w) # mostra os matches do catalogo redmapper em relacao ao wen
-#print idx, d2d, d3d #imprimi os valores
-plot(d2d,d3d) # grafico do redmapper em relacao ao wen
-pylab.xlabel('angle') #legenda pro eixo x
-pylab.ylabel('quantity') #legenda pro eixo y
-pylab.show() # mostra o grafico
+idx, d2d, d3d = cw.match_to_catalog_sky(cr) 
+#print idx, d2d, d3d 
 
 
-c_w = SkyCoord(ra = w['col2']*u.deg, dec = w['col3']*u.deg, distance = cosmo.comoving_distance(w['col4']))  # Pega os valores de ra e dec e z, transformando o z em distancia
-c_r = SkyCoord(ra = r['RA']*u.deg, dec = r['DEC']*u.deg, distance = cosmo.comoving_distance(r['Z_LAMBDA'])) # Pega os valores de ra e dec e z, transformando o z em distancia
+print 'match to catalog sky'
+idx, d2d, d3d = cr.match_to_catalog_sky(cw) 
+#print idx, d2d, d3d 
 
-idx, d2d, d3d = c_w.match_to_catalog_3d(c_r) #mostra os matches do catalogo de wen em relacao ao redmapper
-#print idx, d2d, d3d #imprimi os valores
-plot(d2d,d3d) # grafico do wen em relacao ao redmapper
-pylab.xlabel('angle') #legenda pro eixo x
-pylab.ylabel('quantity') #legenda pro eixo y
-pylab.show() # mostra o grafico
+print 'catalog with values RA, DEC and z'
+c_w = SkyCoord(ra = w['RAJ2000']*u.deg, dec = w['DEJ2000']*u.deg, distance = cosmo.comoving_distance(w['zph'])) 
+c_r = SkyCoord(ra = r['RA']*u.deg, dec = r['DEC']*u.deg, distance = cosmo.comoving_distance(r['Z_LAMBDA'])) 
 
-idx, d2d, d3d = c_r.match_to_catalog_3d(c_w) #mostra os matches  do catalogo de redmapper em relacao ao wen
-#print idx, d2d, d3d # imprimi os valores
-plot(d2d,d3d) #grafico do redmapper em relacao ao wen
-pylabel.xlabel('angle') #legenda pro eixo x
-pylabel.ylabel('quantity') #legenda pro eixo y
-pylab.show() # mostra o grafico
+print 'match to catalog 3d'
+idx, d2d, d3d = c_w.match_to_catalog_3d(c_r) 
+#print idx, d2d, d3d 
 
+print 'match to catalog 3d'
+idx, d2d, d3d = c_r.match_to_catalog_3d(c_w) 
+#print idx, d2d, d3d 
 
-idxc_w, idxc_r, d2d, d3d = c_r.search_around_sky(c_w, 4*u.arcmin) # mostra os objetos em volta do catalogo de wen em relacao ao redmapper
-#print idxc_w, idxc_r, d2d, d3d # imprimi os valores
+print 'Objects around to catalog sky'
+idxcw, idxcr, d2d, d3d = cr.search_around_sky(cw, 4*u.arcmin)
+#print idxc_w, idxc_r, d2d, d3d 
 
-idxc_w, idxc_r, d2d, d3d = c_w.search_around_sky(c_r, 4*u.arcmin) # mostra os objetos em volta do catalogo do redmapper em relacao ao wen
-#print idxc_w, idxc_r, d2d, d3d # imprimi os valores
+idxcw, idxcr, d2d, d3d = cw.search_around_sky(cr, 4*u.arcmin)
+#print idxc_w, idxc_r, d2d, d3d 
 
-idxc_w, idxc_r, d2d, d3d = c_r.search_around_3d(c_w, 1000*u.kpc) # mostra os objetos em volta do catalogo de wen em relacao ao redmapper
-#print idxc_w, idexc_r, d2d, d3d #imprimi os valores
+print 'Objects around to catalog 3d'
+idxc_w, idxc_r, d2d, d3d = c_r.search_around_3d(c_w, 1000*u.kpc) 
+#print idxc_w, idexc_r, d2d, d3d
 
-idxc_w, idxc_r, d2d, d3d = c_w.search_around_3d(c_r, 1000*u.kpc) # mostra os objetos em volta do catalogo do redmapper em relacao ao wen
-#print idxc_w, idexc_r, d2d, d3d #imprimi os valores 
-
-
+idxc_w, idxc_r, d2d, d3d = c_w.search_around_3d(c_r, 1000*u.kpc)
+#print idxc_w, idexc_r, d2d, d3d  
